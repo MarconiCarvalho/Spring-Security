42261e27a967ccc3fd2e0e5cf57aae623b60f6c5
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2025-02-26 22:52:06 -0300

    created models, repositories, service, controller and securityConfiguration

---
+++ auth-api-master/src/main/java/com/example/auth/domain/user/UserRole.java
@@ -0,0 +1,14 @@
+package com.example.auth.domain.user;
+
+public enum UserRole {
+
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role){
+
+        this.role = role;
+    }
+}
