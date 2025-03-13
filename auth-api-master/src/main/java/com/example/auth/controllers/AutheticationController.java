9a1a650e36becf1fd5b5c7e416828bb297a5f3ca
MarconiDev <marconi.carvalho.bcc@gmail.com>
2025-03-13 13:48:45 -0300

    finished project

--- auth-api-master/src/main/java/com/example/auth/controllers/AutheticationController.java
+++ auth-api-master/src/main/java/com/example/auth/controllers/AutheticationController.java
@@ -1,9 +1,11 @@
 package com.example.auth.controllers;
 
 import com.example.auth.domain.user.AuthenticationDTO;
+import com.example.auth.domain.user.LoginResponseDTO;
 import com.example.auth.domain.user.RegisterDTO;
 import com.example.auth.domain.user.User;
 import com.example.auth.repositories.UserRepository;
+import com.example.auth.service.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,27 +24,37 @@ public class  AutheticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+
     }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+
         if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
 
         String  encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+
         User newUser = new User(data.login(), encryptedPassword, data.role());
 
         this.repository.save(newUser);
 
         return ResponseEntity.ok().build();
+
     }
 }
